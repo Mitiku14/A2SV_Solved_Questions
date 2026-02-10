662fd448ace98326c89ea61fafb700c9c5c56cee
Mitiku tadesse <149384297+Mitiku14@users.noreply.github.com>
2026-02-11 00:31:46 +0300

    Add twoSum method to Solution class
    Implement twoSum method to find indices of two numbers that add up to target.

---
+++ Two_Sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_map = {} # dictionary 
+        for i, num in enumerate(nums):
+            compliment = target - num
+            if compliment in num_map:
+                return [num_map[compliment], i] 
+            num_map[num] = i
