faa4a55169f4652cc1a4bde36233be01a14ed74e
Mitiku tadesse <149384297+Mitiku14@users.noreply.github.com>
2026-03-04 09:20:00 +0300

    Add checkSubarraySum method to Solution class
    Implement a solution to check for a continuous subarray sum that equals a multiple of k.

---
+++ Continuous_Subarray_Sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        prefix = {0: -1}
+        cur_sum = 0
+        for i, num in enumerate(nums):
+            cur_sum += num
+            if k != 0:
+                cur_sum %= k
+            if cur_sum in prefix:
+                if i - prefix[cur_sum] > 1:
+                    print(cur_sum)
+                    return True
+            else:
+                prefix[cur_sum] = i
+        
+
+        return False
