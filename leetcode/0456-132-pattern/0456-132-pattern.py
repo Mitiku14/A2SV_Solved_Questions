0d5f77b11ac42f6408452bb9c90a247136eab5d1
Mitiku tadesse <149384297+Mitiku14@users.noreply.github.com>
2026-03-11 19:32:16 +0300

    Add solution for 132 Pattern

---
+++ leetcode/0456-132-pattern/0456-132-pattern.py
@@ -0,0 +1,12 @@
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        stack = []
+        second = float('-inf')
+        for num in reversed(nums):
+            if num < second:
+                return True
+            while stack and num > stack[-1]:
+                second = stack.pop()
+            
+            stack.append(num)
+        return False
